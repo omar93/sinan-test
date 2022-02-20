6234148bcd716df039bd2a09bf982ff238036678
Omar Alhamad <omar93@hotmail.se>
2022-02-20 21:26:50 +0100

    changed root get to file

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ app.use((req, res, next) => {
   console.log(`${req.method}  ${req.url} `, req.params);
   next();
 });
+app.get('*', (req,res) => {
+  res.sendFile(path.join("./build", "index.html"))
+})
 app.use(express.json());
 app.use(cors());
 app.use(express.static(staticFolder));
