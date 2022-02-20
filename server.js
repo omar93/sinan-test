5d761e474a3a6abdb3212b28d4ce3a965193fb50
Omar Alhamad <omar93@hotmail.se>
2022-02-20 21:38:44 +0100

    d

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
   next();
 });
 app.get('*', (req,res) => {
-  res.sendFile(path.join("./build", "index.html"))
+  res.sendFile(staticFolder, "index.html")
 })
 app.use(express.json());
 app.use(cors());
